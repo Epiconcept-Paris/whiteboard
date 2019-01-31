eefc260720cbc153ea0cc3ad8655d7b028f95e11
epi-veh <v.ellayah@epiconcept.io>
2019-01-31 10:55:13 +0400

    - refactoring and linting

--- server/sendData.js
+++ server/sendData.js
@@ -1,30 +1,29 @@
 exports.sendData = function(path){
-  return new Promise((resolve,reject)=>{
+  return new Promise((resolveSendData)=>{
     const fs = require('fs')
     , ws = path.shift()
-    , aRaws = path
+    , aRows = path;
 
-    const tableName = aRaws[0].split("[")[0]
-    aRet = []
+    const tableName = aRows[0].split("[")[0];
 
     fs.readFile(`../data/fod/workspaces/${ws}/dataset/${tableName}.json`, 'utf8', function (err,data) {
       if (err) {
         return console.log(err);
       }
-      const aData = JSON.parse(data)
+      const aData = JSON.parse(data);
       const aRet = aData.map(line=>{
-        let newLine = {}
-        aRaws.forEach(colName =>{
-          let id = colName.split("[")
-          id.shift()
-          let sId = id.toString()
-          sId = sId.substr(0, sId.length -1)
-          newLine[sId] = line[sId]
-        })
-        return newLine
-      })
-      const retJson = JSON.stringify(aRet)
-      resolve(retJson)
+        let newLine = {};
+        aRows.forEach(colName =>{
+          let id = colName.split("[");
+          id.shift();
+          let sId = id.toString();
+          sId = sId.substr(0, sId.length -1);
+          newLine[sId] = line[sId];
+        });
+        return newLine;
+      });
+      const retJson = JSON.stringify(aRet);
+      resolveSendData(retJson);
     })
   })
-}
+};
